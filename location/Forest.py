fca5632bcddcc52a8cabbabd24cecd87b32a0bb8
roman.dobrobabin <roman.dobrobabin@sumsub.com>
2024-01-17 21:26:51 +0200

    x

--- location/Forest.py
+++ location/Forest.py
@@ -2,9 +2,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 class Forest:
-    __to_hunt = KeyboardButton(text='/hunt')
-    __to_shop = KeyboardButton(text='/shop')
-    __resurrect = KeyboardButton(text='/resurrect')
-    __info = KeyboardButton(text='/info')
+    __to_hunt = KeyboardButton(text='/Охотиться')
+    __to_shop = KeyboardButton(text='/Магазин')
+    __resurrect = KeyboardButton(text='/Возродиться')
+    __info = KeyboardButton(text='/Информация')
     keyboard = ReplyKeyboardMarkup(keyboard=[[__to_shop, __to_hunt, __resurrect, __info]], resize_keyboard=True)
 
--- location/shop.py
+++ location/shop.py
@@ -2,6 +2,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 class Shop:
-    __return = KeyboardButton(text='/back')
-    __buyHealth = KeyboardButton(text='/buyhealthpotion')
+    __return = KeyboardButton(text='/Назад')
+    __buyHealth = KeyboardButton(text='/КупитьЗельеЗдоровья')
     keyboard = ReplyKeyboardMarkup(keyboard=[[__return, __buyHealth]], resize_keyboard=True)
